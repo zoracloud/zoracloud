1be2dde8ae9d753584f7888dbc65c74be04e46c2
rflowplatform@gmail.com <allankiplangat22@gmail.com>
2021-09-23 12:30:54 +0900

    Added blueprints to the crudbacknd package

--- components/crud-web-apps/base/backend/zoracloud/crud_backend/__init__.py
+++ components/crud-web-apps/base/backend/zoracloud/crud_backend/__init__.py
@@ -1,6 +1,14 @@
 import logging
 from flask import Flask
 
+from .authn import bp as authn_bp
+from .config import BackendMode
+from .csrf import bp as csrf_bp
+from .errors import bp as errors_bp
+from .probes import bp as probes_bp
+from .routes import bp as base_routes_bp
+from .serving import bp as serving_bp
+
 LOG_FORMAT = "%()s | %(name)s | %(levelname)s | %(message)s"
 
 
@@ -16,5 +24,11 @@ def create_app(name, static_folder, config):
         log.warn("RUNNING IN DEVELOPMENT MODE")
 
     # Register the blueprints
+    app.register_blueprint(authn_bp)
+    app.register_blueprint(errors_bp)
+    app.register_blueprint(csrf_bp)
+    app.register_blueprint(probes_bp)
+    app.register_blueprint(serving_bp)
+    app.register_blueprint(base_routes_bp)
 
     return app
