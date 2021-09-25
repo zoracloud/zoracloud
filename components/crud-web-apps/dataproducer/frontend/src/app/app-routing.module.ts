59d0e695a369d931ca87d295f76b1cb8e44bbbaa
rflowplatform@gmail.com <allankiplangat22@gmail.com>
2021-09-25 17:56:25 +0900

    generate index component

--- components/crud-web-apps/dataproducer/frontend/src/app/app-routing.module.ts
+++ components/crud-web-apps/dataproducer/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { IndexComponent } from './pages/index/index.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: IndexComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
