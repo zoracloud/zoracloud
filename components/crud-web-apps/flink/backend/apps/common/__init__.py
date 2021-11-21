5c863a12653b85ae2f8b1b8517c022ffb322fc69
Allan Chepkoy <chepkoyallan@icloud.com>
2021-11-21 22:31:09 +0900

    Bootstraping common backends

---
+++ components/crud-web-apps/flink/backend/apps/common/__init__.py
@@ -0,0 +1,20 @@
+import zoracloud.zoracloud.crud_backend as base
+
+
+from zoracloud.zoracloud.crud_backend import config, logging
+
+from .routes import bp as routes_bp
+
+log = logging.getLogger(__name__)
+
+
+def create_app(name=__name__, static_folder="static",
+               cfg: config.Config = None):
+    cfg = config.Config() if cfg is None else cfg
+
+    app = base.create_app(name, static_folder, cfg)
+
+    # Register the app's blueprints
+    app.register_blueprint(routes_bp)
+
+    return app
